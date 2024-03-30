336a600d87486d98f693fd66ce8e6beec08742ca
Jeffrey Chen <support-jec@hotmail.com>
2024-03-30 19:14:59 +0800

    重构代码

---
+++ handyhelpers/aliyun/CommandHelper.py
@@ -0,0 +1,10 @@
+class RunCommandHelper:
+    def __init__(self):
+        self.region_id = ""
+        pass
+
+    def syncRun(self, instance_id, cmd_content, timeout):
+        pass
+
+    def asyncRun(self, instance_id, cmd_content, timeout):
+        pass
---
+++ handyhelpers/aliyun/ECSManager.py
@@ -0,0 +1,8 @@
+from .ECSInstance import ECSInstance
+
+class ECSManager:
+    def __init__(self):
+        pass
+    
+    def run_instance(self):
+        return ECSInstance.from_json("")
