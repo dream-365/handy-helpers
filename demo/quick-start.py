ef2841a4cf05198a0ef72a57aae4fb5625b473f6
notebook <notebook@email.com>
2024-04-03 20:09:26 +0800

    更新quick-start代码

---
+++ demo/quick-start.py
@@ -0,0 +1,14 @@
+from handyhelpers.aliyun.ECSManager import ECSManager
+from handyhelpers.aliyun.RunECSInstanceSettings import RunECSInstanceSettings
+import handyhelpers.scripts.linux.alilinux3.docker as docker_scripts
+
+ecs_manager = ECSManager(region_id='cn-hangzhou')
+
+instance = ecs_manager.runInstance(RunECSInstanceSettings(
+    host_name="tf-notebook-cpu", 
+    instance_type="ecs.g7.xlarge",
+    spot_strategy="SpotAsPriceGo"
+    # image_id="aliyun_3_9_x64_20G_uefi_alibase_20231219.vhd"
+    ))
+
+instance.syncRun(docker_scripts.install_docker)
